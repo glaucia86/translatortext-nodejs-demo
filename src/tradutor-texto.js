9710fa7abbf345ba92e122424adef38148667b46
glaucia86 <glaucia_lemos86@hotmail.com>
2019-10-04 19:06:30 -0400

    Alterando novamente a estrutura do projeto.

--- src/tradutor-texto.js
+++ src/tradutor-texto.js
@@ -1,8 +1,10 @@
 /**
+ * 
  * Arquivo: src/tradutor-texto.js
  * Descrição: arquivo responsável por traduzir textos usando o Translator Text API.
  * Data: 03/10/2019
  * Author: Glaucia Lemos
+ * 
  */
 
 

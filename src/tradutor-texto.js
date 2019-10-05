b29b64408af4d6019906bebaa7d751deb10c9dbc
glaucia86 <glaucia_lemos86@hotmail.com>
2019-10-04 22:53:38 -0400

    Alterações no arquivo 'tradutor-texto.js'

--- src/tradutor-texto.js
+++ src/tradutor-texto.js
@@ -7,5 +7,22 @@
  * 
  */
 
+const request = require('request');
+const uuidv4 = require('uuidv4');
 
+let chave_translator = 'TRANSLATOR_TEXT_SUBSCRIPTION_KEY';
 
+//==> Se não encontrar uma variável de ambiente' enviar mensagem de erro!
+if (!process.env[chave_translator]) {
+  throw new Error('Por favor, configure a sua variável de ambiente: ' + chave_translator);
+}
+
+let subscriptionKey = process.env[chave_translator];
+
+let endpoint_translator = 'TRANSLATOR_TEXT_ENDPOINT';
+
+if (!process.env[endpoint_translator]) {
+  throw new Error('Por favor, configure a sua variável de ambiente: ' + endpoint_translator);
+}
+
+let endpoint = process.env[endpoint_translator];
